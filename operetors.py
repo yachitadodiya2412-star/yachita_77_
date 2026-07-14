6a4e18b881129f1ae7368617ec6a1372b5ccc438
yachitadodiya2412-star <yachitadodiya2412@gmail.com>
2026-07-14 17:48:19 +0530

    Add files via upload

---
+++ operetors.py
@@ -0,0 +1,32 @@
+#python operators
+
+#arithmetic operators
+
+num1 = 10
+num2 = 3
+
+print("addition",num1 + num2)
+print("subtaction:",num1 - num2)
+print("multiplication:",num1 * num2)
+print("division:",num1/num2)
+print("modulus:",num1%num2)
+print("expontiotions",num1**num2)
+print("floordivision:",num1//num2)
+
+#assignement operators
+
+x = 10
+x += 2
+#x = x+ 2
+print(x)
+x-=3
+print(x)
+x*=2
+print(x)
+x/=3
+print(x)
+x%=1
+print(x)
+x**=2
+print(x)
+
