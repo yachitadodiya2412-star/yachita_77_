6113b7a395352a2a4734736932eaaf11b7c09607
yachitadodiya2412-star <yachitadodiya2412@gmail.com>
2026-07-14 17:46:26 +0530

    Add files via upload

---
+++ control flow python.py
@@ -0,0 +1,51 @@
+#control flow in python
+
+#conditional control flow
+
+'''
+1.if...statement
+2.if...else statement
+3.if...elif..else statement
+4.match case statement
+'''
+
+#if statement
+
+age=20
+
+if(age>=19):
+    print("aduit")
+
+#if...else statment
+
+age=12
+if(age>=18):
+    print("aduit")
+else:
+    print("minor")
+'''
+#voting programe
+
+age=18
+card=true
+if(age>=18):
+    if(card==true):
+    print("you are eligible for vote")
+'''
+#if../elif..else statement
+
+marks=79
+
+if(marks>=90):
+    print("grade A")
+
+elif(marks>=75):
+    print("grade B")
+
+elif(marks>=60):
+    print("grade C")
+
+else:
+    print("fail")
+
+    
