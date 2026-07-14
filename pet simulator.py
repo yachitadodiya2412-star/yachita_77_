a4162b4ddbc26ca419c958ce2631740fb101a057
yachitadodiya2412-star <yachitadodiya2412@gmail.com>
2026-07-14 17:51:11 +0530

    Add files via upload

---
+++ pet simulator.py
@@ -0,0 +1,13 @@
+#pet simulator
+
+pet = input("what pet do you have (dog/cat/fish)?:")
+
+if pet=="dog":
+    print("woof! i wants treet.")
+elif pet=="cat":
+    print("meow! i rule this home.")
+elif pet=="fish":
+    print("blub blub.... i am swimming in style.")
+
+else:
+    print("that's unique pet!")
