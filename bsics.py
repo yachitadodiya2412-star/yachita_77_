8a8dc345edcf883cbf599a17cc43a2cd9374448a
yachitadodiya2412-star <yachitadodiya2412@gmail.com>
2026-07-14 17:47:29 +0530

    Add files via upload

---
+++ bsics.py
@@ -0,0 +1,27 @@
+#python bsics
+
+print("hello world")
+
+#add two numbars
+
+a=10
+b=20
+c=a+b
+
+print(" sum of :", c )
+
+#user inputs
+
+name = input(" what is your name ")
+
+print(name)
+
+#user input with calculations
+
+a = int(input("enter your 1st nambar = "))
+b = int(input("enter your 2nd nambar = "))
+
+print("addition",a+b)
+print("substriction",a-b)
+print("multiplication",a*b)
+print("division",a/b)
