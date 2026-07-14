3b13c25d967e5904b8228246a827c6dc61167963
yachitadodiya2412-star <yachitadodiya2412@gmail.com>
2026-07-14 17:45:09 +0530

    Add files via upload

---
+++ food maker.py
@@ -0,0 +1,10 @@
+#python basic progaram
+
+# silly sandwich maker
+
+bread = input("choose your bread (white/brown):")
+filling = input("choose yourfilling(chessy/jelly/banana):")
+
+print("hear is your silly sendwich")
+
+print(f"{bread}bread{filling}extra chizzy!!!")
